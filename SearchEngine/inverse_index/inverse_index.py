ce22221810762a87df08fa526fbffadee806690a
Kristiyan Tsvetanov <kris_cvetanov@abv.bg>
2024-02-01 00:19:42 +0200

    Faster ranking and bug fixes.

--- SearchEngine/inverse_index/inverse_index.py
+++ SearchEngine/inverse_index/inverse_index.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from nltk.corpus import PlaintextCorpusReader
 
 from SearchEngine.inverse_index.preprocessing.tokenizer import tokenize_document
@@ -5,16 +7,12 @@ from SearchEngine.inverse_index.preprocessing.tokenizer import tokenize_document
 documents = dict()
 index = dict()
 
-CORPUS_DIR = '../resources/'
+CORPUS_DIR = '../resources/corpus'
 
 """
 Initialize the `documents` dictionary that would be:
-    doc number -> doc content
-To get document content, use the `read_document` function in io-util.py
-Consider what "document content" means, i.e.:
-    - raw document
-    - document with preprocessed words (if we are going to store terms in normalized form). Then,
-      if we want to display original document to user, just read it again.
+    doc_id -> (dictionary of:
+                    term -> number of occurrences of term in doc)
 :param paths: a list of paths to documents
 :return: Initialized `documents`
 """
@@ -26,22 +24,22 @@ def init_documents():
     # corpus.paras returns all paragraphs as each paragraph in corpus.txt is separate document
     raw_documents = corpus.paras()
     for i in range(len(raw_documents)):
-        documents[i] = tokenize_document(raw_documents[i])
+        tokenized = tokenize_document(raw_documents[i])
+        documents[i] = Counter(tokenized)
 
     return documents
 
 
 """
-Read content of all docs and preprocess it (i.e. tokenize it, remove stop words, consider form of storing words).
-Then initialize `index` dictionary that would be:
-word (or term) -> list of numbers of the docs that contain word (or term)
+Initialize `index` dictionary that would be:
+term -> list of ids of the docs that contain term
 :return: Initialized `index`
 """
 
 
 def init_index():
     for doc_id in documents:
-        for word in documents[doc_id]:
+        for word, _ in documents[doc_id].items():
             if word in index:
                 index[word].add(doc_id)
             else:
--- SearchEngine/ranking/search_engine.py
+++ SearchEngine/ranking/search_engine.py
@@ -1,4 +1,8 @@
+from collections import Counter
+
 import numpy as np
+
+from SearchEngine.inverse_index.preprocessing.tokenizer import tokenize_query
 
 
 class SearchEngine:
@@ -8,46 +12,57 @@ class SearchEngine:
         self.index = index
 
     def get_best_documents(self, query):
-        candidates = [(doc_id, doc_content) for doc_id, doc_content in self.documents]
+        query_bag = Counter(tokenize_query(query))
+        candidates = []
 
-        vec_query = self.vectorize(query)
-        vec_candidates = list(
-            map(lambda pair: (pair[0], self.vectorize(pair[1])), candidates)
-        )
+        for doc_id, doc_bag in self.documents.items():
+            keys_intersection = self.keys_intersection(query_bag, doc_bag)
+            query_vec = self.vectorize(query_bag, keys_intersection)
+            doc_vec = self.vectorize(doc_bag, keys_intersection)
+            candidates.append((doc_id, self.cosine_similarity(query_vec, doc_vec, keys_intersection)))
 
-        sorted_candidates = sorted(vec_candidates,
-                                   key=lambda v_c: self.cosine_similarity(vec_query, v_c[1]),
+        sorted_candidates = sorted(candidates,
+                                   key=lambda cand: cand[1],
                                    reverse=True)
 
-        return [pair[0] for pair in sorted_candidates][:10]
+        # return [cand[0] for cand in sorted_candidates][:10]
+        return sorted_candidates[:10]
 
     def tf_idf(self, term, bag):
         return self.tf(term, bag) * self.idf(term)
 
     def tf(self, term, bag):
-        return sum([1 if term == word else 0 for word in bag])
+        return bag[term]
 
     def idf(self, term):
-        docs_content = self.documents.values()
-        df = sum([1 if term in doc else 0 for doc in docs_content])
+        df = len(self.index[term])
 
         return np.log(len(self.documents) / df) if df > 0 else 0
 
-    def vectorize(self, bag):
-        bag_vec = np.zeros(len(self.dictionary))
+    def vectorize(self, bag, keys_intersection):
+        bag_vec = {}
 
-        for term in bag:
+        for term in keys_intersection:
             tf_idf_value = self.tf_idf(term, bag)
             try:
-                index = self.dictionary.index(term)
-                bag_vec[index] = tf_idf_value
+                bag_vec[term] = tf_idf_value
             except ValueError:
                 pass
 
         return bag_vec
 
-    def cosine_similarity(self, query_vec, doc_vec):
-        dot_product = np.dot(query_vec, doc_vec)
-        norm_q = np.linalg.norm(query_vec)
-        norm_d = np.linalg.norm(doc_vec)
-        return dot_product / (norm_q * norm_d)
+    def cosine_similarity(self, query_vec, doc_vec, keys_intersection):
+        dot_product = sum([query_vec[key] * doc_vec[key] for key in keys_intersection])
+        norm_q = np.linalg.norm(list(query_vec.values()))
+        norm_d = np.linalg.norm(list(doc_vec.values()))
+        if norm_d > 0 and norm_q > 0:
+            return dot_product / (norm_q * norm_d)
+        else:
+            return 0
+
+    def keys_intersection(self, query, doc):
+        query_keys = set(query.keys())
+        doc_keys = set(doc.keys())
+        return doc_keys.intersection(query_keys)
+
+
